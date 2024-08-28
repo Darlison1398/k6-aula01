8b72f81d2fe0f5b14f93297b1fcf743effb952d4
Darlison1398 <drsilva123jd@gmail.com>
2024-08-27 22:50:30 -0300

    Aula 01 k6

---
+++ check.js
@@ -0,0 +1,27 @@
+// Import necessary modules
+import { check } from "k6";
+import http from "k6/http";
+
+export default function () {
+  // define URL and request body
+  const url = "https://test-api.k6.io/auth/basic/login/";
+  const payload = JSON.stringify({
+    username: "test_case",
+    password: "1234",
+  });
+  const params = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  // send a post request and save response as a variable
+  const res = http.post(url, payload, params);
+
+  // check that response is 200
+  check(res, {
+    "response code was 200": (res) => res.status == 200,
+  });
+
+  // vai retornar essa resposta:  ✓ response code was 200    junto com outras respostas
+}
